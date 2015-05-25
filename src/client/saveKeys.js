cee5d216d88c61d67a43de4eb12963724884b75e
Craiel <loft45@gmail.com>
2015-05-24 20:40:38 -0400

    Moving into separate repository

---
+++ src/client/saveKeys.js
@@ -0,0 +1,7 @@
+declare('SaveKeys', function() {
+	return {
+        idnSettingsInternalInfoToConsole: StrSha('setIntInfoToConsole'),
+        idnSettingsInternalWarningToConsole: StrSha('setIntWarningToConsole'),
+		idnSettingsInternalLogContexts: StrSha('setIntLogContexts'),
+	};
+});
